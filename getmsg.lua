f6db1aa9b33d72080ef178513969827dd29e63f9
Andre Alves Garzia <andre@andregarzia.com>
2022-01-19 13:39:57 +0000

    consistency in quotes is important

--- getmsg.lua
+++ getmsg.lua
@@ -3,14 +3,15 @@
 -- Requires "supernova" which can be installed from Luarocks
 -- https://luarocks.org/modules/gbaptista/supernova
 
-local utils = require 'utils'
-local supernova = require 'supernova'
+local utils = require "utils"
+local supernova = require "supernova"
 
 local msg, error = ssb:get({id = arg[1]})
 
 if (not error) then
-    print(supernova.italic.yellow('From: ') .. supernova.underline.color("ssb://feed/ed25519/" .. msg.author, '#e317e0'))
-    print(supernova.italic.yellow('Date: ') .. supernova.underline.color(os.date('%A, %B %d %Y at %I:%M:%S %p\n', msg.timestamp//1000), '#e317e0'))
+    local date = os.date("%A, %B %d %Y at %I:%M:%S %p\n", msg.timestamp//1000)
+    print(supernova.italic.yellow("From: ") .. supernova.underline.color(msg.author, "#e317e0"))
+    print(supernova.italic.yellow("Date: ") .. supernova.underline.color(date, "#e317e0"))
 
     print(supernova.gradient(
       utils.textwrap(msg.content.text),
